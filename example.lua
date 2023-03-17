64194b082d222fa519ba23baa87cd3cf5410b2e9
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-17 23:16:05 +0900

    add: example code for current implementation

---
+++ example.lua
@@ -0,0 +1,17 @@
+local asm = require "asm"
+local animationState = require "animationState"
+local dummyPlayer = require "dummyPlayer"
+
+stateMachine = asm.New(dummyPlayer.New())
+
+local fooBar = animationState.New("")
+stateMachine:AddState(fooBar)
+local popup = animationState.New("popup.clip")
+stateMachine:AddState(popup)
+
+stateMachine:AddParameter("stop", false)
+
+stateMachine:AddStartTransition(fooBar)
+stateMachine:AddTransition(transition.New(fooBar, popup, function(params) return params.stop == true end))
+stateMachine:Run()
+stateMachine:Update("stop", true)
