ea58cbe850011024855717dc1af2fd2cae32f476
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-18 23:24:51 +0900

    chore: transition> Rename transitions' "trigger" to "condition"
    It followed Unity's animation term, and avoid naming conflict with
    parameters' "trigger"

--- transition.lua
+++ transition.lua
@@ -4,10 +4,10 @@ transition = {}
 ---Create new transition
 ---@param from animationState
 ---@param to animationState
----@param triggerFunc fun(params: table): boolean
+---@param conditionFunc fun(params: table): boolean
 ---@return transition
-function transition.New(from, to, triggerFunc)
-  local obj = { trigger = triggerFunc
+function transition.New(from, to, conditionFunc)
+  local obj = { condition = conditionFunc
               , from = from
               , to   = to
   }
@@ -17,10 +17,10 @@ function transition.New(from, to, triggerFunc)
     -- Adjust tab indentation
     local from = string.gsub(tostring(self.from), "\n\t", "\n\t\t")
     local to = string.gsub(tostring(self.to), "\n\t", "\n\t\t")
-    local trigger = string.gsub(tostring(self.trigger), "\n\t", "\n\t\t")
+    local condition = string.gsub(tostring(self.condition), "\n\t", "\n\t\t")
 
-    return string.format("(transition :from %s\n\t:to %s\n\t:trigger\n\t%s)"
-                        , from, to, trigger)
+    return string.format("(transition :from %s\n\t:to %s\n\t:condition\n\t%s)"
+                        , from, to, condition)
   end
 
   return setmetatable(obj, {__index=transition, __tostring=toStr})
@@ -37,7 +37,7 @@ end
 ---@param params table
 ---@return boolean
 function transition:Check(params)
-  return self.trigger(params)
+  return self.condition(params)
 end
 
 
