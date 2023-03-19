0e3dbae35bc62477d38e1d915c7eaa35f2d3208e
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-19 16:02:47 +0900

    Change: example> update to use asm

--- example.lua
+++ example.lua
@@ -1,5 +1,8 @@
 local stateMachine = require "stateMachine"
+local dummyPlayer = require "dummyPlayer"
+local asm = require "asm"
 
+-- Creating statemachine {{{
 mySM = stateMachine.New()
 
 local idle = "idle"
@@ -12,27 +15,34 @@ mySM:AddState(transmitting)
 mySM:AddState(shrink)
 
 mySM:AddParameter("transmitting", false)
-mySM:AddParameter("animationEnd",{type = "trigger", status = false})
 mySM:AddParameter("quit", false)
 
 mySM:AddStartTransition(idle)
 mySM:AddTransition(transition.New(idle, popup, function(params) return params.transmitting end))
-mySM:AddTransition(transition.New(popup, transmitting, function(params) return params.animationEnd.state end))
+mySM:AddTransition(transition.New(popup, transmitting, function(params) return params.asm_animationEnd.state end))
 mySM:AddTransition(transition.New(transmitting, shrink, function(params) return not params.transmitting end))
-mySM:AddTransition(transition.New(shrink, idle, function(params) return params.animationEnd.state end))
+mySM:AddTransition(transition.New(shrink, idle, function(params) return params.asm_animationEnd.state end))
 mySM:AddEndTransition(idle, function(params) return params.quit end)
 -- }}}
 
--- execution
-mySM:Run()
+
+myAsm = asm.ConvertFromStateMachine(mySM, dummyPlayer.New())
 print(mySM.currentState)
-mySM:Update("transmitting", true)
-print(mySM.currentState)
-mySM:Update("animationEnd", {}) -- any value can be supplied
-print(mySM.currentState)
-mySM:Update("transmitting", false)
-print(mySM.currentState)
-mySM:Update("animationEnd", {}) -- any value can be supplied
-print(mySM.currentState)
-mySM:Update("quit", true)
-print(mySM.currentState)
+local animCo = myAsm:Run()
+
+local eventCo = coroutine.create(function()
+      for j=1, 3 do coroutine.yield() end
+      myAsm:Update("transmitting", true)
+      for j=1, 10 do coroutine.yield() end
+      myAsm:Update("transmitting", false)
+      for j=1, 3 do coroutine.yield() end
+      print("[eventCo] finished")
+      myAsm:Update("quit", true)
+end)
+
+
+for i = 1, 30 do
+   coroutine.resume(animCo)
+   coroutine.resume(eventCo)
+   print(myAsm.currentState)
+end 
